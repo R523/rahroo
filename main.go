8677f10e4bb0f63dba229d58a72622e72da0a03f
Parham Alvani <parham.alvani@gmail.com>
2021-10-16 21:41:03 +0000

    feat: make it works

--- main.go
+++ main.go
@@ -1,39 +1,66 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/pterm/pterm"
-	"golang.org/x/exp/io/spi"
+	"github.com/stianeikeland/go-rpio/v4"
 )
 
-// Channel indicates MCP3008 input channel.
-const Channel = 0
+const (
+	// Channel indicates MCP3008 input channel.
+	Channel uint8 = 0
+	// Command sends over spi interface
+	// nolint: gomnd
+	Command uint8 = (8 + Channel) << 4
+)
 
 func main() {
 	if err := pterm.DefaultBigText.WithLetters(
-		pterm.NewLettersFromStringWithStyle("Ra", pterm.NewStyle(pterm.FgCyan)),
-		pterm.NewLettersFromStringWithStyle("hroo", pterm.NewStyle(pterm.FgLightRed)),
+		pterm.NewLettersFromStringWithStyle("Rah", pterm.NewStyle(pterm.FgCyan)),
+		pterm.NewLettersFromStringWithStyle("roo", pterm.NewStyle(pterm.FgLightRed)),
 	).Render(); err != nil {
 		_ = err
 	}
 
-	dev, err := spi.Open(&spi.Devfs{
-		Dev:      "/dev/spidev0.0",
-		Mode:     spi.Mode0,
-		MaxSpeed: 3600000,
-	})
-	if err != nil {
-		pterm.Fatal.Printf("cannot open spi device 0.0 %s\n", err)
+	if err := rpio.Open(); err != nil {
+		pterm.Error.Printf("falied to open rpio %s", err)
+
+		return
 	}
-	defer dev.Close()
+	defer rpio.Close()
 
-	txbuf := []byte{0x01, (8 + Channel) << 4, 0x00}
-	rxbuf := make([]byte, 3)
+	if err := rpio.SpiBegin(rpio.Spi0); err != nil {
+		pterm.Error.Printf("falied to open spi0 %s", err)
 
-	if err := dev.Tx(txbuf, rxbuf); err != nil {
-		pterm.Fatal.Printf("cannot read from spi %s\n", err)
+		return
 	}
+	defer rpio.SpiEnd(rpio.Spi0)
 
-	value := uint16(rxbuf[1]&0x03)<<8 | uint16(rxbuf[2])
+	rpio.SpiChipSelect(0)
 
-	pterm.Success.Printf("%d\n", value)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-quit:
+			return
+		case <-ticker.C:
+			data := []byte{0x01, Command, 0x00}
+
+			rpio.SpiExchange(data)
+
+			// nolint: gomnd
+			value := uint16(data[1]&0x03)<<8 | uint16(data[2])
+
+			pterm.Info.Println(value)
+		}
+	}
 }
